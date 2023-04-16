5f65406995a509fbefa5c75ee35f4aec4fca2910
PrytMaks <prytkovmaks@gmail.com>
2023-04-16 23:27:25 +0500

    feat:

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  while(l.value == k){
+    l =  l.next;
+  };
+
+  return l;
 }
 
 module.exports = {
